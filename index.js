27e2349cceb7b6b731816d8190c636dfe0880a10
ManuelSuarez <roblecito2023@gmail.com>
2024-07-17 20:53:02 -0300

    arregle estilos cuando la pantalla tenga menos de 600px

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.body.style.zoom = "80%";
+// document.body.style.zoom = "80%";
 //turn pages when click next or prev button
 const pageTurnBtn = document.querySelectorAll(".nextprev-btn");
 
